0826b24ea8ab5c16b3bfaa8275603cfe64d143b4
LFalch <lucas@wasd.dk>
2020-09-24 03:02:54 +0200

    Make images

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::collections::HashSet;
 use std::env::args;
 
+use image::GrayImage;
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 pub struct SpaceInvader(pub u128);
@@ -21,34 +22,73 @@ impl SpaceInvader {
     }
 }
 
+// TODO use clap for argument parsing instead
+
 fn main() -> Result<(), i32> {
-    let (width, mut start) = match &*args().skip(1).collect::<Vec<_>>() {
-        [] => (0, SpaceInvader(0)),
-        [w] => {
+    // main_program(args().skip(1).collect::<Vec<_>>())
+    for w in 1..128 {
+        main_program(vec![format!("out{}.png", w), format!("{}", w), "10000".to_owned()])?;
+    }
+    Ok(())
+}
+
+fn main_program(args: Vec<String>) -> Result<(), i32> {
+    let mut limit = None;
+    let (out_path, width, mut start) = match &*args {
+        [out_path, w] => {
             let w = w.parse().map_err(|_| 2)?;
-            (w, SpaceInvader((0b1 << w as u128) - 1))
+            (out_path, w, SpaceInvader((0b1 << w as u128) - 1))
         }
-        [w, start] => {
+        [out_path, w, length_limit] => {
+            limit = Some(length_limit.parse().map_err(|_| 4)?);
             let w = w.parse().map_err(|_| 2)?;
-            (w, SpaceInvader(start.parse().map_err(|_| 3)?))
+            (out_path, w, SpaceInvader((0b1 << w as u128) - 1))
         }
-        [_, _, ..] => {
-            eprintln!("Too many args");
+        [out_path, w, length_limit, start] => {
+            limit = Some(length_limit.parse().map_err(|_| 4)?);
+            let w = w.parse().map_err(|_| 2)?;
+            (out_path, w, SpaceInvader(start.parse().map_err(|_| 3)?))
+        }
+        _ => {
+            eprintln!("Wrong amount of args");
             return Err(1);
         }
     };
 
-    let mut set = HashSet::with_capacity(width as usize);
+    if width > 127 {
+        eprintln!("Can't do that");
+        return Err(10);
+    }
 
+    let mut set = HashSet::with_capacity(width as usize);
+    let mut rows = Vec::with_capacity(width as usize);
+
+    println!("{:01$b}", start.0, width as usize);
     loop {
-        println!("{:01$b}", start.0, width as usize);
+        rows.push(start.0);
         if !set.insert(start) {
-            break
+            break;
+        }
+        if let Some(limit) = limit {
+            if rows.len() >= limit {
+                break;
+            }
         }
 
         start = start.next(width);
     }
+    drop(set);
+    let mut img = GrayImage::new(width as u32, rows.len() as u32);
 
+    for (row, y) in rows.into_iter().zip(0..) {
+        for i in 0..(width as u32) {
+            let bit = (row >> (width - i as u8 - 1)) & 1;
+            // println!("{}, {}: {}", i, y, bit);
+            img.put_pixel(i, y, [(bit == 0) as u8 * 0xff].into())
+        }
+    }
+
+    img.save(out_path).unwrap();
 
     Ok(())
 }
